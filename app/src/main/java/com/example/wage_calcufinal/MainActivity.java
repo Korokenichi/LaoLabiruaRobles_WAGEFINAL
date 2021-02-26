d79d520c61e2e72f997bde6a7b2a7f80672cb944
Korokenichi <79259221+Korokenichi@users.noreply.github.com>
2021-02-26 14:45:43 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/wage_calcufinal/MainActivity.java
+++ app/src/main/java/com/example/wage_calcufinal/MainActivity.java
@@ -4,53 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements MainActivity1 {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int op1 = 0;
+    int op1more = 0;
+    int wage;
+    int overwage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText txtOperand1;
+        TextView txtAnswer1, txtAnswer2, txtAnswer3, txtAnswer4;
+
+        txtOperand1 = findViewById(R.id.txtOperand1);
+        txtAnswer1 = findViewById(R.id.txtAnswer1Hours);
+        txtAnswer2 = findViewById(R.id.txtAnswer2Over);
+        txtAnswer3 = findViewById(R.id.txtAnswer3Wage);
+        txtAnswer4 = findViewById(R.id.txtAnswer4OW);
+
+        txtAnswer1.setText("-");
+        txtAnswer2.setText("-");
+        txtAnswer3.setText("-");
+        txtAnswer4.setText("-");
+
+        txtOperand1.setInputType(InputType.TYPE_CLASS_NUMBER);
+
         Button btnCalcu;
 
         btnCalcu = findViewById(R.id.btnCalcu);
 
-        btnCalcu.setOnClickListener((View.OnClickListener) this);
+        btnCalcu.setOnClickListener(this);
     }
     @Override
     public void onClick(View v) {
 
         EditText txtOperand1;
         TextView txtAnswer1, txtAnswer2, txtAnswer3, txtAnswer4;
+        Button HoursTXT, WageTXT, OvrTmeHTXT, OvrTmeWTXT;
 
         txtOperand1 = findViewById(R.id.txtOperand1);
-        txtAnswer1 = findViewById(R.id.txtAnswer1);
-        txtAnswer2 = findViewById(R.id.txtAnswer2);
-        txtAnswer3 = findViewById(R.id.txtAnswer3);
-        txtAnswer4 = findViewById(R.id.txtAnswer4);
+        txtAnswer1 = findViewById(R.id.txtAnswer1Hours);
+        txtAnswer2 = findViewById(R.id.txtAnswer2Over);
+        txtAnswer3 = findViewById(R.id.txtAnswer3Wage);
+        txtAnswer4 = findViewById(R.id.txtAnswer4OW);
 
-        int op1 = 0;
+        txtOperand1.setInputType(InputType.TYPE_CLASS_NUMBER);
 
         op1 = Integer.parseInt(txtOperand1.getText().toString());
 
-        switch (v.getId()) {
-            case R.id.btnCalcu:
-                int wage;
-                if (op1 > 8) {
+        if (v.getId() == R.id.btnCalcu) {
+            if (op1 > 0) {
+                if (op1 <= 8) {
                     wage = op1 * 100;
-                } else if (op1 > 8) {
-                    op1 -= 8;
-                    wage = 800 + (op1 * 130);
-                    txtAnswer1.setText("Hours:" + (op1 + 8));
-                    txtAnswer2.setText("Total wage" + wage);
-                    txtAnswer3.setText("Overtime Hours" + txtOperand1);
-                    txtAnswer4.setText("Overtime Wage" + op1 * 130);
-                } else {
-                    txtAnswer1.setText("Invalid Input"); }
+                    op1more = 0;
+                    overwage = 0;
+                }
+                else if (op1 > 8) {
+                    op1more = op1 - 8;
+                    wage = 800 + (op1more * 130);
+                    overwage = wage - 800;
+                }
+                txtAnswer1.setText(Integer.toString(op1));
+                txtAnswer2.setText(Integer.toString(op1more));
+                txtAnswer3.setText(Integer.toString(wage) + " PHP");
+                txtAnswer4.setText(Integer.toString(overwage) + " PHP");
+            }
+            if (op1 <= 0 || op1 > 24) {
+                txtAnswer1.setText("Invalid");
+                txtAnswer2.setText("Invalid");
+                txtAnswer3.setText("Invalid");
+                txtAnswer4.setText("Invalid");
+            }
             }
         }
     }
